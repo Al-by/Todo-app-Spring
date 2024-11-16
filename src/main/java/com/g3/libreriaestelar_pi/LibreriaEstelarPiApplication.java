c9c30c9942d9f4f372f5a9526de88f40fb926d86
Al_by <edu.123gts@gmail.com>
2024-11-16 18:15:46 -0500

    v0.1 Base de proyecto

---
+++ src/main/java/com/g3/libreriaestelar_pi/LibreriaEstelarPiApplication.java
@@ -0,0 +1,13 @@
+package com.g3.libreriaestelar_pi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaEstelarPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaEstelarPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g3/libreriaestelar_pi/LibreriaEstelarPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g3.libreriaestelar_pi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaEstelarPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
