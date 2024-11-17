41ff998757774a9678c188321449f57b1ef8e035
joellarp17 <i202223973@cibertec.edu.pe>
2024-11-16 21:53:37 -0500

    implementacion login

---
+++ src/main/java/com/g3/libreriaestelar_pi/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.g3.libreriaestelar_pi.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.g3.libreriaestelar_pi.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	Optional<Usuario> findByEmail(String email);
+
+}
---
+++ src/main/java/com/g3/libreriaestelar_pi/security/Auth.java
@@ -0,0 +1,11 @@
+package com.g3.libreriaestelar_pi.security;
+
+import lombok.Data;
+
+@Data
+public class Auth {
+	
+	private String email;
+	private String password;
+
+}
