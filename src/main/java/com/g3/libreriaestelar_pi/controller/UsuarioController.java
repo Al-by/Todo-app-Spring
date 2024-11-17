77ef98f3acb6253f8601d2cba3fa20d8a9f2f40c
Belenn333 <kiaraferh@gmail.com>
2024-11-17 02:04:08 -0500

    Aplicando primeras validaciones

--- src/main/java/com/g3/libreriaestelar_pi/controller/UsuarioController.java
+++ src/main/java/com/g3/libreriaestelar_pi/controller/UsuarioController.java
@@ -3,11 +3,11 @@ package com.g3.libreriaestelar_pi.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.g3.libreriaestelar_pi.dto.UsuarioRegistroDTO;
-import com.g3.libreriaestelar_pi.model.Usuario;
 import com.g3.libreriaestelar_pi.service.UsuarioService;
 
 @RestController
@@ -19,7 +19,15 @@ public class UsuarioController {
 
     @PostMapping("/register")
     public ResponseEntity<String> registrarUsuario(@Valid @RequestBody UsuarioRegistroDTO usuarioDTO) {
-        usuarioService.registrarUsuario(usuarioDTO);
-        return ResponseEntity.ok("Usuario creado satisfactoriamente");
+    	try {
+            usuarioService.registrarUsuario(usuarioDTO);
+            return ResponseEntity.ok("Usuario creado satisfactoriamente");
+        } catch (IllegalArgumentException e) {
+            // Devuelve un mensaje de error con código 400
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            // Devuelve un mensaje genérico de error con código 500
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocurrió un error inesperado");
+        }
     }
 }
