e9ae4ee056a969dc055cd64819f25ddc328d7e60
javieropepo <javier140605@gmail.com>
2024-11-18 17:35:17 -0500

    Envio de errores a front

--- src/main/java/com/g3/libreriaestelar_pi/controller/UsuarioController.java
+++ src/main/java/com/g3/libreriaestelar_pi/controller/UsuarioController.java
@@ -1,12 +1,10 @@
 package com.g3.libreriaestelar_pi.controller;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import com.g3.libreriaestelar_pi.dto.UsuarioRegistroDTO;
 import com.g3.libreriaestelar_pi.service.UsuarioService;
 
@@ -19,16 +17,41 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> registrarUsuario(@Valid @RequestBody UsuarioRegistroDTO usuarioDTO) {
-    	try {
+    public ResponseEntity<Object> registrarUsuario(@Valid @RequestBody UsuarioRegistroDTO usuarioDTO) {
+        try {
             usuarioService.registrarUsuario(usuarioDTO);
             return ResponseEntity.ok("Usuario creado satisfactoriamente");
         } catch (IllegalArgumentException e) {
             // Devuelve un mensaje de error con código 400
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            // Devuelve un mensaje genérico de error con código 500
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocurrió un error inesperado");
+            ErrorResponse errorResponse = new ErrorResponse("VALIDATION_ERROR", e.getMessage());
+            return ResponseEntity.badRequest().body(errorResponse);
+        }
+    }
+
+    // Clase estática para la estructura de respuesta de error
+    public static class ErrorResponse {
+        private String errorType;
+        private String message;
+
+        public ErrorResponse(String errorType, String message) {
+            this.errorType = errorType;
+            this.message = message;
+        }
+
+        public String getErrorType() {
+            return errorType;
+        }
+
+        public void setErrorType(String errorType) {
+            this.errorType = errorType;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
         }
     }
 }
